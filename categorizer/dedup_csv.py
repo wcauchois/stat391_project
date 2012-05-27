356c9c76e1f7cb1c3581422cd9ccbde5b77b0d40
Bill Cauchois <wcauchois@gmail.com>
2012-05-27 13:48:17 -0700

    dedup classified.csv with new script

---
+++ categorizer/dedup_csv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from config import CLASSIFIED_CSV
+from loaddata import read_csv
+
+COLUMNS = ['name', 'text', 'label']
+
+if __name__ == '__main__':
+  print ','.join(COLUMNS)
+  data = read_csv(open(CLASSIFIED_CSV, 'r'))
+  seen_text = set()
+  for datum in data:
+    if 'text' not in datum:
+      continue
+    if datum['text'] in seen_text:
+      continue
+    else:
+      seen_text.add(datum['text'])
+    row = []
+    for col in COLUMNS:
+      row.append(datum[col])
+    print ','.join(row)
